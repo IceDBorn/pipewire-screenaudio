958f75b8a96fde2745b2d586d462b61fe22aca3d
jim3692 <31220180+jim3692@users.noreply.github.com>
2023-04-01 11:25:58 +0300

    Update message-parser.c

--- native/message-parser.c
+++ native/message-parser.c
@@ -9,16 +9,7 @@ int main (int argc, char *argv[]) {
   for (int i = 0; i < INT_SIZE; i++)
     buffer[i] = getchar();
 
-  /*
-  for (int i = 0; i < INT_SIZE / 2; i++) {
-    unsigned char temp = buffer[i];
-    buffer[i] = buffer[INT_SIZE - 1 - i];
-    buffer[INT_SIZE - 1 - i] = temp;
-  }
-  */
-
   int charactersToRead = *(int*)buffer;
-  // printf("charactersToRead: %d", charactersToRead);
   free(buffer);
 
   buffer = (unsigned char*)malloc(charactersToRead + 1);
