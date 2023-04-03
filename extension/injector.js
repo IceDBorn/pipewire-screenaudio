abfb97b8cdb223b81bd20639a4b041ecfa4b6ca1
jim3692 <jim3692@gmail.com>
2023-04-03 18:47:25 +0300

    Comments, messages

--- extension/injector.js
+++ extension/injector.js
@@ -1,19 +1,16 @@
 const nullthrows = (v) => {
-  if (v == null) throw new Error("it's a null")
+  if (v == null) throw new Error('null')
   return v
 }
 
 function injectCode (src) {
   const script = document.createElement('script')
-  // This is why it works!
   script.src = src
   script.onload = function () {
-    console.log('script injected')
+    console.log('screenaudio-mic script injected')
     this.remove()
   }
 
-  // This script runs before the <head> element is created,
-  // so we add the script to <html> instead.
   nullthrows(document.head || document.documentElement).appendChild(script)
 }
 
