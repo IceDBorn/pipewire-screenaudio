5a2e9e6e83aa690de7c00140c9051d7ffe8e622a
IceDBorn <github.envenomed@dralias.com>
2023-04-06 03:22:43 +0300

    Use bootstrap for UI

--- extension/scripts/popup.js
+++ extension/scripts/popup.js
@@ -2,7 +2,8 @@ const MESSAGE_NAME = 'com.icedborn.pipewirescreenaudioconnector'
 const ALL_DESKTOP_AUDIO_TEXT = 'All Desktop Audio'
 
 const dropdown = document.getElementById('dropdown')
-const heading = document.getElementById('heading')
+const message = document.getElementById('message')
+const reload = document.getElementById('reload-btn')
 
 let selectedNode = null
 
@@ -24,7 +25,7 @@ async function isRunning () {
 function createShareBtn (root) {
   const shareBtn = document.createElement('button')
   shareBtn.id = 'share-btn'
-  shareBtn.className = 'pure-material-button-contained'
+  shareBtn.className = 'btn btn-success'
   shareBtn.innerText = 'Share'
   root.appendChild(shareBtn)
 
@@ -43,7 +44,7 @@ function createShareBtn (root) {
 function createStopBtn (root) {
   const stopBtn = document.createElement('button')
   stopBtn.id = 'stop-btn'
-  stopBtn.className = 'pure-material-button-contained'
+  stopBtn.className = 'btn btn-danger mt-3'
   stopBtn.innerText = 'Stop'
   root.appendChild(stopBtn)
 
@@ -63,12 +64,15 @@ function createStopBtn (root) {
 
 async function updateGui (root) {
   if (await isRunning()) {
-    heading.innerText = `Pipewire Screenaudio is running with PID: ${window.localStorage.getItem('micPid')}`
+    message.innerText = `Running with PID: ${window.localStorage.getItem('micPid')}`
+    message.hidden = false
     dropdown.hidden = true
+    reload.hidden = true
     createStopBtn(root)
   } else {
-    heading.innerText = 'Select audio node to share'
+    message.hidden = true
     dropdown.hidden = false
+    reload.hidden = false
     createShareBtn(root)
   }
 }
@@ -97,15 +101,13 @@ function onResponse (response) {
 
   const root = document.getElementById('root')
   updateGui(root)
-
-  heading.innerText = 'Select audio node to share'
 }
 
 function onError (error) {
   console.error(error)
-  heading.innerText = 'The native connector is missing!'
+  message.innerText = 'The native connector is misconfigured or missing!'
   dropdown.hidden = true
+  reload.hidden = true;
 }
 
-const sending = chrome.runtime.sendNativeMessage(MESSAGE_NAME, { cmd: 'GetNodes', args: [] })
-sending.then(onResponse, onError)
+chrome.runtime.sendNativeMessage(MESSAGE_NAME, { cmd: 'GetNodes', args: [] }).then(onResponse, onError)
