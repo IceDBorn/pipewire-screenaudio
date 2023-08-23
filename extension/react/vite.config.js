38c0c564f2af7d6fec7f7e16a9e5028d0dee866f
jim3692 <jim3692@gmail.com>
2023-08-23 09:38:28 +0300

    Vite confing Prettier format + Enable sourcemaps

--- extension/react/vite.config.js
+++ extension/react/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   base: "/react/dist",
   esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment'
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
   },
   css: {
     modules: {
-      localsConvention: 'camelCaseOnly'
-    }
-  }
-})
+      localsConvention: "camelCaseOnly",
+    },
+  },
+  build: {
+    sourcemap: "inline",
+  },
+});
