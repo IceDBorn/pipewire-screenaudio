46255fdb4367049cf011f6cd350a1b75218377d0
jim3692 <jim3692@gmail.com>
2023-08-23 09:53:22 +0300

    Switch from entr to nodemon

--- extension/react/build-watcher.sh
+++ extension/react/build-watcher.sh
@@ -5,5 +5,5 @@ scriptRoot="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 cd $scriptRoot
 
 # Watch for file changes
-ls | entr -sc "npx vite build && notify-send 'Pipewire Screenaudio' 'Extension rebuilt'"
+npx nodemon --ignore ./dist/ --exec "npx vite build && notify-send 'Pipewire Screenaudio' 'Extension rebuilt'"
 
