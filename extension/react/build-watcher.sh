41d3e5e9f026d004d1befb52d887dbf702b19d0d
IceDBorn <github.envenomed@dralias.com>
2023-10-15 18:47:15 +0300

    Add image to extension builder notification

--- extension/react/build-watcher.sh
+++ extension/react/build-watcher.sh
@@ -5,5 +5,4 @@ scriptRoot="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 cd $scriptRoot
 
 # Watch for file changes
-npx nodemon -e js,jsx --ignore ./dist/ --exec "npx vite build && notify-send 'Pipewire Screenaudio' 'Extension rebuilt'"
-
+npx nodemon -e js,jsx --ignore ./dist/ --exec "npx vite build && notify-send -i $scriptRoot/../assets/icons/icon.svg 'Pipewire Screenaudio' 'Extension rebuilt'"
