6adb6cc761dd1611ec9c48369c901b9a5b8cce7b
jim3692 <jim3692@gmail.com>
2023-10-22 12:37:35 +0300

    Add error on missing message

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,13 +1,16 @@
 // Any event, that is handled in here, should have a comment with the reason it is handled in here
 
-let commandsQueue = []
-let commandsQueueRunning = false
+let commandsQueue = [];
+let commandsQueueRunning = false;
 
-function sendNativeMessage (messageName, cmd, args) {
-  return chrome.runtime.sendNativeMessage(messageName, { cmd, args: (args ? [args] : undefined) })
+function sendNativeMessage(messageName, cmd, args) {
+  return chrome.runtime.sendNativeMessage(messageName, {
+    cmd,
+    args: args ? [args] : undefined,
+  });
 }
 
-async function runQueuedCommands () {
+async function runQueuedCommands() {
   commandsQueueRunning = true;
 
   while (commandsQueue.length) {
@@ -21,13 +24,21 @@ async function runQueuedCommands () {
       });
     }
 
-    console.log(args)
-    const result = await sendNativeMessage(command.messageName, command.cmd, args, command.maps)
-    console.log(result)
+    console.log(args);
+    const result = await sendNativeMessage(
+      command.messageName,
+      command.cmd,
+      args,
+      command.maps,
+    );
+    console.log(result);
 
     if (outMap) {
       outMap.forEach(([storageKey, resultKey]) => {
-        window.localStorage.setItem(storageKey, (resultKey ? result[resultKey] : null));
+        window.localStorage.setItem(
+          storageKey,
+          resultKey ? result[resultKey] : null,
+        );
       });
     }
 
@@ -40,17 +51,25 @@ async function runQueuedCommands () {
 }
 
 function handleMessage(request) {
-  // Run multiple commands sequentially
-  if (request.message === "enqueue-command") {
-    commandsQueue.push({ ...request.command, messageName: request.messageName })
-    if (!commandsQueueRunning) {
-      runQueuedCommands();
-    }
-  }
+  switch (request.message) {
+    // Run multiple commands sequentially
+    case "enqueue-command":
+      commandsQueue.push({
+        ...request.command,
+        messageName: request.messageName,
+      });
+      if (!commandsQueueRunning) {
+        runQueuedCommands();
+      }
+      break;
 
-  // Called from injector.js - It cannot directly call sendNativeMessage
-  if (request.message === "get-session-type") {
-    return sendNativeMessage(request.messageName, "GetSessionType");
+    // Called from injector.js - It cannot directly call sendNativeMessage
+    case "get-session-type":
+      sendNativeMessage(request.messageName, "GetSessionType");
+      break;
+
+    default:
+      throw `Unknown message "${request.message}"`;
   }
 }
 
