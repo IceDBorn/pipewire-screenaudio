bfd543fdab36a725553bbc73171c8e13b6a976db
Alan Sartorio <alan42ga@hotmail.com>
2023-10-21 21:05:43 -0300

    Move port search for screenaudio node to lua

---
+++ native/connector/find-ports-by-node-id.lua
@@ -0,0 +1,30 @@
+#!/usr/bin/env /usr/bin/wpexec
+utils = require("utils"):Configure(Constraint, Log, Interest)
+
+argv = ...
+
+targetId = argv.targetId
+
+if targetId == nil then
+  Core.quit()
+  return
+end
+
+node_finder = ObjectManager {
+  Interest {
+    type = "node",
+    Constraint { "object.id", "=", targetId, type = "pw" },
+  },
+}
+
+node_finder:connect("installed", function()
+  node = node_finder:lookup()
+
+  if node ~= nil then
+    utils:PrintPorts(node)
+  end
+
+  Core.quit()
+end)
+
+node_finder:activate()
--- native/connector/virtmic.sh
+++ native/connector/virtmic.sh
@@ -4,44 +4,28 @@ source $PROJECT_ROOT/connector/util.sh
 
 exec 1>>`UtilGetLogPathForFile $(basename $0)` 2>&1
 
-# Get all nodes to check if $VIRTMIC_NODE_NAME exists, and create it
-pw-dump |
-    jq --exit-status -c -s "flatten(1) | [ .[] | select(.info.props[\"node.name\"] == \"$VIRTMIC_NODE_NAME\") ][0]" >/dev/null && (
-      UtilLog "[virtmic.sh] [Found Node] $VIRTMIC_NODE_NAME"
-    ) || (
-      pw-cli create-node adapter "{ factory.name=support.null-audio-sink node.name=$VIRTMIC_NODE_NAME media.class=Audio/Source/Virtual object.linger=1 audio.position=[FL,FR] }"
-      UtilLog "[virtmic.sh] [Created Node] $VIRTMIC_NODE_NAME"
-    )
 
-fullDumpFile=`UtilGetTempFile`
+# Check if $VIRTMIC_NODE_NAME exists, and create it
+virtmicId=`./find-screenaudio-node.lua`
+if [[ -z "$virtmicId" ]]; then
+    pw-cli create-node adapter "{ factory.name=support.null-audio-sink node.name=$VIRTMIC_NODE_NAME media.class=Audio/Source/Virtual object.linger=1 audio.position=[FL,FR] }"
+    virtmicId=`./wait-screenaudio-node.lua`
+    UtilLog "[virtmic.sh] [Created Node] $VIRTMIC_NODE_NAME (id = $virtmicId)"
+else
+    UtilLog "[virtmic.sh] [Found Node] $VIRTMIC_NODE_NAME (id = $virtmicId)"
+fi
 
-# === Collect required data from PipeWire === #
-# Get all nodes again for further processing
-pw-dump | jq -s "flatten(1)" > $fullDumpFile
-UtilLog "[virtmic.sh] [Got Dump] File: $fullDumpFile"
+read -r virtmicPortFlId virtmicPortFrId <<< `./find-ports-by-node-id.lua targetId="$virtmicId"`
 
-# Get id and ports of $VIRTMIC_NODE_NAME
-virtmicId=`cat "$fullDumpFile" | jq -c "[ .[] | select(.info.props[\"node.name\"] == \"$VIRTMIC_NODE_NAME\") ][0].id"`
-UtilLog "[virtmic.sh] [Got Id] $virtmicId"
-
-virtmicPortsFile=`UtilGetTempFile`
-cat "$fullDumpFile" | jq -c "[ .[] | select(.info.direction == \"input\") | select(.info.props[\"node.id\"] == $virtmicId) ]" > $virtmicPortsFile
-UtilLog "[virtmic.sh] [Got Ports] File: $virtmicPortsFile"
-
-virtmicPortFlId=`cat "$virtmicPortsFile" | jq -c "[ .[] | select(.info.props[\"audio.channel\"] == \"FL\") ][0].id"`
-virtmicPortFrId=`cat "$virtmicPortsFile" | jq -c "[ .[] | select(.info.props[\"audio.channel\"] == \"FR\") ][0].id"`
 UtilLog "[virtmic.sh] [Got Ports] FL: $virtmicPortFlId"
 UtilLog "[virtmic.sh] [Got Ports] FR: $virtmicPortFrId"
-
-rm $virtmicPortsFile
-UtilLog "[virtmic.sh] [Cleared Ports] File: $virtmicPortsFile"
 
 fifoPath=`UtilGetFifoPath "$virtmicId"`
 mkfifo "$fifoPath"
 UtilLog "[virtmic.sh] [Created FIFO] $fifoPath"
 
 # Cleanup on exit
-trap "rm $fullDumpFile $fifoPath; kill -- -$CURRENT_PID" EXIT
+trap "rm $fifoPath; kill -- -$CURRENT_PID" EXIT
 
 function disconnectInputs() {
     node=$1
