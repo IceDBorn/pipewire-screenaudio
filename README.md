4b81551e8d8983c80a9c3b56446dc03fbb9dce85
IceDBorn <git.outsider841@simplelogin.fr>
2023-11-27 00:43:44 +0200

    Fix jq not working correctly

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # <img src="./extension/assets/icons/icon.svg" width="22" alt="Logo"> Pipewire Screenaudio
+
 Extension to passthrough Pipewire audio to WebRTC Screenshare
 
 Based on [link-app-to-mic](https://github.com/Soundux/rohrkabel/tree/master/examples/link-app-to-mic) and [Screenshare-with-audio-on-Discord-with-Linux](https://github.com/edisionnano/Screenshare-with-audio-on-Discord-with-Linux)
 
 ## Communication
+
 You can find us on [Matrix](https://matrix.to/#/#pipewire-screenaudio:matrix.org)
 
 ## Installation
+
 ### Packages
+
 [![AUR](https://img.shields.io/aur/version/pipewire-screenaudio?style=for-the-badge)](https://aur.archlinux.org/packages/pipewire-screenaudio)
 [![AUR](https://img.shields.io/aur/version/pipewire-screenaudio-git?style=for-the-badge)](https://aur.archlinux.org/packages/pipewire-screenaudio-git)
+
 #### NixOS Flakes
+
 ```nix
 # flake.nix
 
@@ -25,7 +31,7 @@ You can find us on [Matrix](https://matrix.to/#/#pipewire-screenaudio:matrix.org
         ./configuration.nix
       ];
     };
-  } 
+  }
 }
 
 # configuration.nix
@@ -39,33 +45,43 @@ You can find us on [Matrix](https://matrix.to/#/#pipewire-screenaudio:matrix.org
 ```
 
 ### Installing from Source
+
 #### Requirements
+
 - gawk
 - hexdump
 - jq
 - pipewire
-- psmisc
 
 ```bash
 git clone https://github.com/IceDBorn/pipewire-screenaudio.git
 cd pipewire-screenaudio
 bash install.sh
 ```
+
 ## Usage
+
 - Install the [extension](https://addons.mozilla.org/firefox/addon/pipewire-screenaudio)
 - Optional: Grant extension with access permissions to all sites
 - Join a WebRTC call, click the extension icon, select an audio node and share
 - Stream, your transmission should contain both audio and video
- 
+
 ## Known Problems
+
 - You can't stream firefox WebRTC calls at all while using `All Desktop Audio`, they are excluded by default
+
 ### resistFingerprinting
+
 - privacy.resistFingerprinting (enabled by default in LibreWolf, arkenfox user.js, etc.) breaks the extension. Either disable the preference or add any domains you wish to use Pipewire Screenaudio with to `privacy.resistFingerprinting.exemptedDomains` in `about:config`
+
 ### Audio pitching
+
 > Note: The bug is now fixed in the nightly branch of Firefox. This is set to release to the stable branch on December 5th!
+
 - ~~Firefox recently implemented a feature for spawning WebRTC audio nodes, and while it works, it has a lot of pitching and de-sync issues. We created a [bug report](https://bugzilla.mozilla.org/show_bug.cgi?id=1844181) on bugzilla and we're hoping for the best!~~
 
 ## Planned Features
+
 - Multiple nodes selection
 - More customization options (node matching, watcher behavior etc.)
 - Chromium support
--- flake.nix
+++ flake.nix
@@ -2,52 +2,53 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   description = "The native part of the Pipewire Screenaudio extension";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    pkgsFor = nixpkgs.legacyPackages;
-    systems = ["aarch64-linux" "i686-linux" "x86_64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-    mkDate = longDate:
-      with builtins; (concatStringsSep "-" [
-      (substring 0 4 longDate)
-      (substring 4 2 longDate)
-      (substring 6 2 longDate)
-      ]);
-  in {
-    packages = forAllSystems (system: {
-      default = with pkgsFor.${system};
-        stdenv.mkDerivation {
-          name = "pipewire-screenaudio";
-          version = mkDate (self.lastModifiedDate or "19700101") + "_" + (self.shortRev or "dirty");
+  outputs = { self, nixpkgs, }:
+    let
+      pkgsFor = nixpkgs.legacyPackages;
+      systems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      mkDate = longDate:
+        with builtins;
+        (concatStringsSep "-" [
+          (substring 0 4 longDate)
+          (substring 4 2 longDate)
+          (substring 6 2 longDate)
+        ]);
+    in {
+      packages = forAllSystems (system: {
+        default = with pkgsFor.${system};
+          stdenv.mkDerivation {
+            name = "pipewire-screenaudio";
+            version = mkDate (self.lastModifiedDate or "19700101") + "_"
+              + (self.shortRev or "dirty");
 
-          src = self;
+            src = self;
 
-          buildInputs = [
-            gawk
-            hexdump
-            jq
-            pipewire
-            psmisc
-          ];
+            buildInputs = [ gawk hexdump jq pipewire ];
 
-          installPhase = ''
-            runHook preInstall
+            installPhase = ''
+              runHook preInstall
 
-            mkdir -p $out/lib/out
-            install -Dm755 native/connector/pipewire-screen-audio-connector.sh $out/lib/connector/pipewire-screen-audio-connector.sh
-            install -Dm755 native/connector/virtmic.sh $out/lib/connector/virtmic.sh
-            install -Dm755 native/connector/connect-and-monitor.sh $out/lib/connector/connect-and-monitor.sh
-            install -Dm755 native/connector/util.sh $out/lib/connector/util.sh
+              # Replace jq with its absolute path
+              substituteInPlace native/connector/virtmic.sh --replace jq ${pkgs.jq}/bin/jq
+              substituteInPlace native/connector/pipewire-screen-audio-connector.sh --replace jq ${pkgs.jq}/bin/jq
+              substituteInPlace native/connector/connect-and-monitor.sh --replace jq ${pkgs.jq}/bin/jq
+              substituteInPlace native/connector/util.sh --replace jq ${pkgs.jq}/bin/jq
 
-            # Firefox manifest
-            sed -i "s|/usr/lib/pipewire-screenaudio|$out/lib|g" native/native-messaging-hosts/firefox.json
-            install -Dm644 native/native-messaging-hosts/firefox.json $out/lib/mozilla/native-messaging-hosts/com.icedborn.pipewirescreenaudioconnector.json
+              # Install files
+              mkdir -p $out/lib/out
+              install -Dm755 native/connector/pipewire-screen-audio-connector.sh $out/lib/connector/pipewire-screen-audio-connector.sh
+              install -Dm755 native/connector/virtmic.sh $out/lib/connector/virtmic.sh
+              install -Dm755 native/connector/connect-and-monitor.sh $out/lib/connector/connect-and-monitor.sh
+              install -Dm755 native/connector/util.sh $out/lib/connector/util.sh
 
-            runHook postInstall
-          '';
-        };
-    });
-  };
+              # Firefox manifest
+              substituteInPlace native/native-messaging-hosts/firefox.json --replace /usr/lib/pipewire-screenaudio $out/lib
+              install -Dm644 native/native-messaging-hosts/firefox.json $out/lib/mozilla/native-messaging-hosts/com.icedborn.pipewirescreenaudioconnector.json
+
+              runHook postInstall
+            '';
+          };
+      });
+    };
 }
