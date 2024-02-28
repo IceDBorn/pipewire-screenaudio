03db3a3addaa46479ee08a464f9118e83a5c0e87
jim3692 <jim3692@gmail.com>
2024-02-28 23:14:14 +0200

    Refactor GetNodes

---
+++ native/connector-rs/src/helpers.rs
@@ -0,0 +1,37 @@
+use std::vec::Vec;
+
+extern crate json;
+use json::JsonValue;
+
+extern crate log;
+use log::debug;
+
+pub trait JsonGetters {
+  fn get_field_or_fail(&self, field: &str) -> Result<JsonValue,String>;
+  fn get_fields_chain(&self, fields: Vec<&str>) -> Result<JsonValue,String>;
+}
+
+impl JsonGetters for JsonValue {
+  fn get_field_or_fail(&self, field: &str) -> Result<JsonValue,String> {
+    if self.has_key(field) && !self[field].is_null() {
+      Ok(self[field].to_owned())
+    } else {
+      Err(format! ("Field does not exist or null: {}", field))
+    }
+  }
+
+  fn get_fields_chain(&self, fields: Vec<&str>) -> Result<JsonValue,String> {
+    if fields.len() == 0 {
+      return Err("The 'fields' vector is empty.".to_string());
+    }
+
+    debug! ("Testing field: {}", fields[0]);
+
+    if fields.len() > 1 {
+      let value = self.get_field_or_fail(fields[0])?;
+      value.get_fields_chain(fields[1..].to_owned())
+    } else {
+      self.get_field_or_fail(fields[0])
+    }
+  }
+}
