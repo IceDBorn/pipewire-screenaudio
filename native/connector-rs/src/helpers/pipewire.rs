4d2eecc3544e57ca20b7a2a723330a828815ccc8
jim3692 <jim3692@gmail.com>
2024-02-29 01:05:34 +0200

    New fn get_pw_dump

--- native/connector-rs/src/helpers/pipewire.rs
+++ native/connector-rs/src/helpers/pipewire.rs
@@ -9,7 +9,7 @@ use log::debug;
 
 use crate::helpers::JsonGetters;
 
-pub fn get_output_nodes() -> Vec<JsonValue> {
+fn get_pw_dump() -> Vec<JsonValue> {
   let dump_buffer = Command::new("pw-dump")
     .output()
     .expect("failed to execute process")
@@ -18,7 +18,13 @@ pub fn get_output_nodes() -> Vec<JsonValue> {
   let dump_string = str::from_utf8(&dump_buffer).unwrap();
   let dump = json::parse(dump_string).unwrap();
 
-  let dump_filtered = dump.members().filter(|&node| {
+  dump.members().map(|node| node.to_owned()).collect::<Vec<_>>()
+}
+
+pub fn get_output_nodes() -> Vec<JsonValue> {
+  let dump = get_pw_dump();
+
+  let dump_filtered = dump.iter().filter(|&node| {
     let media_class_result = node.get_fields_chain(vec!["info","props","media.class"]);
     if let Err(e) = media_class_result {
       debug! ("Error: {}", e);
