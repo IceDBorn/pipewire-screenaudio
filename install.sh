9a2150643ecdf7320fba0bc4e2737e6df81f2c84
Aviana <winblocker@gmx.de>
2024-03-29 18:53:49 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 projectRoot="$( cd -- "$(dirname "$0")" > /dev/null 2>&1 ; pwd -P )"
 
 mkdir -p ~/.mozilla/native-messaging-hosts
+mkdir -p ~/.config/chromium/NativeMessagingHosts
+mkdir -p ~/.config/google-chrome/NativeMessagingHosts
+mkdir -p ~/.config/microsoft-edge/NativeMessagingHosts
 
 sed "s|/usr/lib/pipewire-screenaudio|$projectRoot/native|g" $projectRoot/native/native-messaging-hosts/firefox.json > ~/.mozilla/native-messaging-hosts/com.icedborn.pipewirescreenaudioconnector.json
+sed "s|/usr/lib/pipewire-screenaudio|$projectRoot/native|g" $projectRoot/native/native-messaging-hosts/chromium.json > ~/.config/chromium/NativeMessagingHosts/com.icedborn.pipewirescreenaudioconnector.json
+sed "s|/usr/lib/pipewire-screenaudio|$projectRoot/native|g" $projectRoot/native/native-messaging-hosts/chromium.json > ~/.config/google-chrome/NativeMessagingHosts/com.icedborn.pipewirescreenaudioconnector.json
+sed "s|/usr/lib/pipewire-screenaudio|$projectRoot/native|g" $projectRoot/native/native-messaging-hosts/chromium.json > ~/.config/microsoft-edge/NativeMessagingHosts/com.icedborn.pipewirescreenaudioconnector.json
 
